a365ce85c90e82f68645a0158f24868f46485150
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2020-05-18 19:16:54 +0530

    sonar qube config

---
+++ src/main/resources/db/migration/V4__insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(username, first_name, last_name) VALUES('callicodeer', 'Rajeegeev', 'Siegngh');
+INSERT INTO users(username, first_name, last_name) VALUES('flywayteste', 'Freglyway', 'Testegge');
